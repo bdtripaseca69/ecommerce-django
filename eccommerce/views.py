abc19280c69a7e55092e4e9f52f83fa57c6127ba
bdtripaseca <ing.martinezbd@gmail.com>
2023-06-23 17:17:59 -0600

    update #223

--- eccommerce/views.py
+++ eccommerce/views.py
@@ -4,7 +4,7 @@ from store.models import Product,  ReviewRating
 
 def home(request):                                               #218, ultima instruccion para que se filtre por la fecha de creacion
     products = Product.objects.all().filter(is_available = True).order_by('created_date') #condicion para que liste solo los que estan disponibles
-    
+    reviews = None
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status = True)
 
