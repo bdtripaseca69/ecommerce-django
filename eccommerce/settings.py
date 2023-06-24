b887d43e37c73fe05384380ea8de0218e046b8e8
bdtripaseca <ing.martinezbd@gmail.com>
2023-06-23 18:55:31 -0600

    otra

--- eccommerce/settings.py
+++ eccommerce/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from decouple import config #219 manejo de libreria que permite el
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent  #223
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY') #219
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default= True)  #219... se manda el parametro para que el dato retornado esta en boolean
 
-ALLOWED_HOSTS = ['eccommerce-env.eba-shpyuktc.us-west-2.elasticbeanstalk.com'] #222 , colocacion del host provicional de aws
-
+ALLOWED_HOSTS = ['eccommerce-env.eba-shpyuktc.us-west-2.elasticbeanstalk.com','*'] #222 , colocacion del host provicional de aws
+#ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'contact_App',
     'orders',
     'admin_honeypot', #219 complementar con un migrate
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -163,11 +164,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+'''STATIC_URL = 'static/'    #225
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS=[
     'eccommerce/static'  #se aplico el comando ...manage.py collectstatic
+]'''
+
+# AWS S3 Static Files Configuration    #225
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    'eccommerce/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'eccommerce.media_storages.MediaStorage'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR /'media'
